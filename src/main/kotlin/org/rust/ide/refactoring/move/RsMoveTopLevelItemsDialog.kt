ffe988bcc4732710a657d3ba647b35dbb1f592cc
Dmitry Murzin <diralik@yandex.ru>
2020-08-27 12:34:22 +0300

    REF: Group structs and traits with related impls in move items dialog

--- src/main/kotlin/org/rust/ide/refactoring/move/RsMoveTopLevelItemsDialog.kt
+++ src/main/kotlin/org/rust/ide/refactoring/move/RsMoveTopLevelItemsDialog.kt
@@ -8,6 +8,7 @@ package org.rust.ide.refactoring.move
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.TextFieldWithBrowseButton
 import com.intellij.openapi.util.Key
+import com.intellij.openapi.util.text.StringUtil
 import com.intellij.openapi.vfs.LocalFileSystem
 import com.intellij.openapiext.isUnitTestMode
 import com.intellij.refactoring.RefactoringBundle.message
@@ -16,6 +17,9 @@ import com.intellij.refactoring.util.CommonRefactoringUtil
 import com.intellij.ui.components.JBTextField
 import com.intellij.ui.layout.panel
 import com.intellij.util.IncorrectOperationException
+import org.apache.commons.lang.StringEscapeUtils
+import org.rust.ide.docs.signature
+import org.rust.lang.core.psi.*
 import org.rust.lang.core.psi.ext.RsItemElement
 import org.rust.lang.core.psi.ext.RsMod
 import org.rust.openapiext.pathToRsFileTextField
@@ -23,6 +27,7 @@ import org.rust.openapiext.toPsiFile
 import org.rust.stdext.mapToSet
 import java.awt.Dimension
 import java.io.File
+import javax.swing.Icon
 import javax.swing.JComponent
 
 class RsMoveTopLevelItemsDialog(
@@ -34,17 +39,18 @@ class RsMoveTopLevelItemsDialog(
     private val sourceFilePath: String = sourceMod.containingFile.virtualFile.path
     private val sourceFileField: JBTextField = JBTextField(sourceFilePath).apply { isEnabled = false }
     private val targetFileChooser: TextFieldWithBrowseButton = createTargetFileChooser(project)
-    private val memberPanel: RsMemberSelectionPanel = createMemberInfoPanel()
+    private val memberPanel: RsMoveMemberSelectionPanel = createMemberSelectionPanel()
 
     private var searchForReferences: Boolean = true
 
     init {
         super.init()
         title = "Move Module Items"
+        validateButtons()
     }
 
     private fun createTargetFileChooser(project: Project): TextFieldWithBrowseButton {
-        return pathToRsFileTextField(disposable, "Choose Destination File", project)
+        return pathToRsFileTextField(disposable, "Choose Destination File", project, ::validateButtons)
             .also {
                 it.text = sourceFilePath
                 it.textField.caretPosition = sourceFilePath.removeSuffix(".rs").length
@@ -52,10 +58,28 @@ class RsMoveTopLevelItemsDialog(
             }
     }
 
-    private fun createMemberInfoPanel(): RsMemberSelectionPanel {
-        val memberInfo = getTopLevelItems()
-            .map { RsMemberInfo(it, itemsToMove.contains(it)) }
-        return RsMemberSelectionPanel("Items to move", memberInfo)
+    private fun createMemberSelectionPanel(): RsMoveMemberSelectionPanel {
+        val topLevelItems = getTopLevelItems()
+
+        val nodesGroupedWithImpls = groupImplsByStructOrTrait(sourceMod, topLevelItems.toSet())
+            .map { RsMoveItemAndImplsInfo(it.key, it.value) }
+        val itemsGroupedWithImpls = nodesGroupedWithImpls.flatMap { it.children }.map { it.member }
+
+        val nodesWithoutGrouping = topLevelItems.subtract(itemsGroupedWithImpls).map { RsMoveMemberInfo(it) }
+        val nodesAll = nodesGroupedWithImpls + nodesWithoutGrouping
+
+        val nodesSelected = nodesAll
+            .flatMap {
+                when (it) {
+                    is RsMoveItemAndImplsInfo -> it.children
+                    is RsMoveMemberInfo -> listOf(it)
+                    else -> error("unexpected node info type: $it")
+                }
+            }
+            .filter { it.member in itemsToMove }
+
+        return RsMoveMemberSelectionPanel(project, "Items to move", nodesAll, nodesSelected)
+            .also { it.tree.setInclusionListener { validateButtons() } }
     }
 
     override fun createCenterPanel(): JComponent {
@@ -71,7 +95,7 @@ class RsMoveTopLevelItemsDialog(
             }
             row {
                 cell(isFullWidth = true) {
-                    checkBox(message("search.for.references"), searchForReferences)
+                    checkBox(message("search.for.references"), ::searchForReferences)
                 }
             }
         }.also { it.preferredSize = Dimension(600, 400) }
@@ -83,8 +107,22 @@ class RsMoveTopLevelItemsDialog(
             .filter { RsMoveTopLevelItemsHandler.canMoveElement(it) }
     }
 
+    override fun areButtonsValid(): Boolean {
+        // `memberPanel` is initialized after `createTargetFileChooser`,
+        // which triggers `areButtonsValid` check
+        @Suppress("SENSELESS_COMPARISON")
+        if (memberPanel == null) return false
+
+        return sourceFilePath != targetFileChooser.text && getSelectedItems().isNotEmpty()
+    }
+
+    private fun getSelectedItems(): Set<RsItemElement> =
+        memberPanel.tree.includedSet
+            .filterIsInstance<RsMoveMemberInfo>()
+            .mapToSet { it.member }
+
     public override fun doAction() {
-        val itemsToMove = memberPanel.table.selectedMemberInfos.mapToSet { it.member }
+        val itemsToMove = getSelectedItems()
         val targetFilePath = targetFileChooser.text
 
         val targetMod = findTargetMod(targetFilePath)
@@ -108,6 +146,38 @@ class RsMoveTopLevelItemsDialog(
         val targetFile = LocalFileSystem.getInstance().findFileByIoFile(File(targetFilePath))
         return targetFile?.toPsiFile(project) as? RsMod
     }
+}
+
+class RsMoveMemberInfo(val member: RsItemElement) : RsMoveNodeInfo() {
+    override val description: String =
+        if (member is RsModItem) {
+            "mod ${member.modName}"
+        } else {
+            val descriptionHTML = buildString { member.signature(this) }
+            val description = StringEscapeUtils.unescapeHtml(StringUtil.removeHtmlTags(descriptionHTML))
+            description.replace("(?U)\\s+".toRegex(), " ")
+        }
+    override val icon: Icon = member.getIcon(0)
+}
+
+class RsMoveItemAndImplsInfo(
+    val item: RsItemElement, // struct or trait
+    val impls: List<RsImplItem>
+) : RsMoveNodeInfo() {
+    override val description: String = run {
+        val name = item.name
+        val keyword = when (item) {
+            is RsStructItem -> "struct"
+            is RsEnumItem -> "enum"
+            is RsTypeAlias -> "type"
+            is RsTraitItem -> "trait"
+            else -> null
+        }
+        if (name != null && keyword != null) "$keyword $name and impls" else "item and impls"
+    }
+    override val icon: Icon? = null
+    override val children: List<RsMoveMemberInfo> = mutableListOf(RsMoveMemberInfo(item))
+        .also { children -> impls.mapTo(children) { RsMoveMemberInfo(it) } }
 }
 
 val MOVE_TARGET_MOD_KEY: Key<RsMod> = Key("RS_MOVE_TARGET_MOD_KEY")
