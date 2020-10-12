1f3aee4d6ee60a7ff1e146b929a65ee8890b6972
mchernyavsky <chernyavsky.mikhail@gmail.com>
2020-10-13 01:57:22 +0300

    Add an extension point for toolchain path suggestion

--- src/main/kotlin/org/rust/cargo/toolchain/RsToolchain.kt
+++ src/main/kotlin/org/rust/cargo/toolchain/RsToolchain.kt
@@ -10,15 +10,16 @@ import com.intellij.openapi.util.io.FileUtil
 import com.intellij.util.io.exists
 import com.intellij.util.text.SemVer
 import org.rust.cargo.toolchain.tools.Cargo
-import org.rust.cargo.toolchain.tools.Rustc
+import org.rust.ide.sdk.flavors.RsSdkFlavor
+import org.rust.ide.sdk.flavors.suggestHomePaths
+import org.rust.stdext.isExecutable
 import java.io.File
-import java.nio.file.Files
 import java.nio.file.Path
 
 data class RsToolchain(val location: Path) {
     val presentableLocation: String = pathToExecutable(Cargo.NAME).toString()
 
-    fun looksLikeValidToolchain(): Boolean = hasExecutable(Cargo.NAME) && hasExecutable(Rustc.NAME)
+    fun looksLikeValidToolchain(): Boolean = RsSdkFlavor.getFlavor(location) != null
 
     // for executables from toolchain
     fun pathToExecutable(toolName: String): Path {
@@ -39,69 +40,18 @@ data class RsToolchain(val location: Path) {
         return cargoBinPath.resolve(exeName).toAbsolutePath()
     }
 
-    fun hasExecutable(exec: String): Boolean = Files.isExecutable(pathToExecutable(exec))
+    fun hasExecutable(exec: String): Boolean = pathToExecutable(exec).isExecutable()
 
-    fun hasCargoExecutable(exec: String): Boolean = Files.isExecutable(pathToCargoExecutable(exec))
+    fun hasCargoExecutable(exec: String): Boolean = pathToCargoExecutable(exec).isExecutable()
 
     companion object {
         val MIN_SUPPORTED_TOOLCHAIN = SemVer.parseFromText("1.32.0")!!
 
-        fun suggest(): RsToolchain? = Suggestions.all().mapNotNull {
-            val candidate = RsToolchain(it.toPath().toAbsolutePath())
-            if (candidate.looksLikeValidToolchain()) candidate else null
-        }.firstOrNull()
-    }
-}
-
-private object Suggestions {
-    fun all() = sequenceOf(
-        fromRustup(),
-        fromPath(),
-        forMac(),
-        forUnix(),
-        forWindows()
-    ).flatten()
-
-    private fun fromRustup(): Sequence<File> {
-        val file = File(FileUtil.expandUserHome("~/.cargo/bin"))
-        return if (file.isDirectory) {
-            sequenceOf(file)
-        } else {
-            emptySequence()
-        }
-    }
-
-    private fun fromPath(): Sequence<File> = System.getenv("PATH").orEmpty()
-        .split(File.pathSeparator)
-        .asSequence()
-        .filter { !it.isEmpty() }
-        .map(::File)
-        .filter { it.isDirectory }
-
-    private fun forUnix(): Sequence<File> {
-        if (!SystemInfo.isUnix) return emptySequence()
-
-        return sequenceOf(File("/usr/local/bin"))
-    }
-
-    private fun forMac(): Sequence<File> {
-        if (!SystemInfo.isMac) return emptySequence()
-
-        return sequenceOf(File("/usr/local/Cellar/rust/bin"))
-    }
-
-    private fun forWindows(): Sequence<File> {
-        if (!SystemInfo.isWindows) return emptySequence()
-        val fromHome = File(System.getProperty("user.home") ?: "", ".cargo/bin")
-
-        val programFiles = File(System.getenv("ProgramFiles") ?: "")
-        val fromProgramFiles = if (!programFiles.exists() || !programFiles.isDirectory)
-            emptySequence()
-        else
-            programFiles.listFiles { file -> file.isDirectory }.asSequence()
-                .filter { it.nameWithoutExtension.toLowerCase().startsWith("rust") }
-                .map { File(it, "bin") }
-
-        return sequenceOf(fromHome) + fromProgramFiles
+        fun suggest(): RsToolchain? =
+            RsSdkFlavor.getApplicableFlavors()
+                .asSequence()
+                .flatMap { it.suggestHomePaths().asSequence() }
+                .map { RsToolchain(it.toAbsolutePath()) }
+                .firstOrNull()
     }
 }
